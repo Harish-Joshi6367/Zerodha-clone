36c0d763d667fc28649b4fc66fb6c5b9bfa68ff9
Harish-Joshi6367 <Harisharma6367@gmail.com>
2024-09-23 19:29:36 +0530

    support-section completed

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -2,7 +2,66 @@ import React from 'react';
 
 function CreateTicket() {
     return (
-        <h1>createTicket</h1>
+        <div className='container'>
+            <div className='row mb-5'>
+                <p className='fs-4 text-muted mt-5 mb-2' style={{paddingLeft:"100px"}}>To create a ticket, select a relevant topic</p>
+            </div>
+            <div className='row mb-4'>
+                <div className='col-4' style={{paddingLeft:"100px"}}>
+                    <a href="" style={{textDecoration:"none", color:"black"}}><p className='fs-5'><i class="fa-solid fa-user-plus"></i>&nbsp; Account Opening</p></a>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Getting started</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Online</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Offline</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Charges</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Company, Partnership and HUF</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Non Resident Indian (NRI)</a>
+                </div>
+                <div className='col-4' style={{paddingLeft:"100px"}}>
+                    <a href="" style={{textDecoration:"none", color:"black"}}><p className='fs-5'><i class="fa-regular fa-user"></i>&nbsp; Your Zerodha Account</p></a>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Login credentials</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Your Profile</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Account modification and segment addition</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>CMR & DP ID</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Nomination</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Transfer and conversion of shares</a>
+                </div>
+                <div className='col-4' style={{paddingLeft:"100px"}}>
+                    <a href="" style={{textDecoration:"none", color:"black"}}><p className='fs-5'><i class="fa-solid fa-chart-line"></i>&nbsp; Trading and Markets</p></a>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Trading FAQs</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Kite</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Margins</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Product and order types</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Corporate actions</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Kite features</a>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-4' style={{paddingLeft:"100px"}}>
+                    <a href="" style={{textDecoration:"none", color:"black"}}><p className='fs-5'><i class="fa-solid fa-coins"></i>&nbsp; Coin</p></a>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Understanding mutual funds and Coin</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Coin app</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Coin web</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Transactions and reports</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>National Pension Scheme (NPS)</a><br></br>
+                </div>
+                <div className='col-4' style={{paddingLeft:"100px"}}>
+                    <a href="" style={{textDecoration:"none", color:"black"}}><p className='fs-5'><i class="fa-brands fa-ussunnah"></i>&nbsp; Funds</p></a>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Fund withdrawal</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Adding funds</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Adding bank accounts</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>eMandates</a><br></br>
+                </div>
+                <div className='col-4' style={{paddingLeft:"100px"}}>
+                    <a href="" style={{textDecoration:"none", color:"black"}}><p className='fs-5'><i class="fa-solid fa-terminal"></i>&nbsp; Console</p></a>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>IPO</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Portfolio</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Funds statement</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Profile</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Reports</a><br></br>
+                    <a href="" style={{textDecoration:"none", textAlign:"left", lineHeight:"30px", fontSize:"15px", paddingLeft:"30px"}}>Referral program</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Hero() {
     return (
-        <h1>Hero</h1>
+        <div className='container1' id="supportHero">
+            <div className='row' style={{color:"white", paddingBottom:"100px"}}>
+                <div className='col-2'></div>
+                <div className='col-5' style={{paddingTop:"70px"}}>
+                    <a href="" style={{textDecoration:"none", color:"white"}}><h4>Support Portal</h4></a><br></br>
+                    <p className='fs-3 mt-2'>Search for an answer or browse help topics to create a ticket</p>
+                    <input style={{lineHeight:"50px"}} type="text" className="form-control mt-4" placeholder='Eg: how do i activate F&O, why is myorder getting rejected' autoFocus></input>
+                    <a href="" style={{color:"white", fontSize:"18px", lineHeight:"60px"}}>Track account opening</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="" style={{color:"white", fontSize:"18px"}}>Track segment activation</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="" style={{color:"white", fontSize:"18px"}}>Intraday margins</a><br></br>
+                    <a href="" style={{color:"white", fontSize:"18px"}}>Kite user manual</a>
+                </div>
+                <div className='col-5' style={{paddingTop:"70px"}}>
+                    <a href="" style={{color:"white", padding:"0 250px"}}>Track Tickets</a><br></br><br></br><br></br><br></br><br></br>
+                    <h3 className='fs-4' style={{marginLeft:"30px"}}>Featured</h3>
+                    <ol style={{paddingLeft:"80px", lineHeight:"40px", fontSize:"17px"}}>
+                        <li><a href="" style={{color:"white"}}>Surveillance measure on scrips - September 2024</a></li>
+                        <li><a href="" style={{color:"white"}}>Rights Entitlements listing in September 2024</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
     );
 }
 
